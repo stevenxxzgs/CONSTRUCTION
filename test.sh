16d3e7076edfd2b54f0d358dffece255a321b103
steven <steven@stevendeMacBook-Air.local>
2024-07-10 16:52:10 +0800

    speedtest

--- test.sh
+++ test.sh
@@ -10,3 +10,5 @@ ffmpeg -rtsp_transport tcp -i "rtsp://admin:xu123456@192.168.1.110:554/cam/realm
 # 声音
 ffmpeg -rtsp_transport tcp -i "rtsp://admin:xu123456@192.168.1.110:554/cam/realmonitor?channel=1&subtype=0" -rtsp_transport tcp -i "rtsp://admin:xu123456@192.168.1.*:554/cam/realmonitor?channel=1&subtype=0" -map 0:v -map 1:a -fiter_complex "[0:v]lenscorrection=cx=0.5:cy=0.5:k1=-0.45555:k2=0.16,rotate=-0.01,crop=iw:ih-10" -t 5 -y -c:v h264_nvenc -c:a aac -b:v 3000k out.mp4
 
+# 测速
+speedtest
